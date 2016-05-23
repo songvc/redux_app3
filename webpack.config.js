cc5c3839b4525fa9cd20e6f806b63ede5f83282d
Vincent Song <songvc@users.noreply.github.com>
2016-05-23 00:23:22 -0700

    search bar building

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query:
+      {
+          presets:['es2015', 'react']
+      }
     }]
   },
   resolve: {
